35981724751f4b0af4e5178131fbba20420cb760
Loresa <loresag17@gmail.com>
2025-04-20 22:28:48 +0200

    Role CRUD back-end

---
+++ hotel-airbnb/src/main/java/com/hotel_airbnb/service/IRoleService.java
@@ -0,0 +1,20 @@
+package com.hotel_airbnb.service;
+
+import com.hotel_airbnb.model.Role;
+import com.hotel_airbnb.model.User;
+
+import java.util.List;
+
+
+
+public interface IRoleService {
+    List<Role> getRoles();
+    Role createRole(Role theRole);
+
+    void deleteRole(Long id);
+    Role findByName(String name);
+
+    User removeUserFromRole(Long userId, Long roleId);
+    User assignRoleToUser(Long userId, Long roleId);
+    Role removeAllUsersFromRole(Long roleId);
+}
