1a8e47ccaf721a50af368301e2292e4ee3ff982c
Loresa <loresag17@gmail.com>
2025-04-20 21:57:20 +0200

    spring initalizr

---
+++ hotel-airbnb/src/main/java/com/hotel_airbnb/HotelAirbnbApplication.java
@@ -0,0 +1,13 @@
+package com.hotel_airbnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelAirbnbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelAirbnbApplication.class, args);
+	}
+
+}
---
+++ hotel-airbnb/src/test/java/com/hotel_airbnb/HotelAirbnbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel_airbnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelAirbnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
